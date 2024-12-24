9564286288896619312a811b73626ccf5fc4149f
AliHassan487 <ah0720487@gmail.com>
2024-12-24 18:00:33 +0500

    design navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,21 +1,41 @@
-'use client';
+"use client";
+
 import Image from "next/image";
-import CustomButton from "./CustomButton";
+import { CustomButton } from ".";
 
 const Hero = () => {
-    const handleScroll = () => {}
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">Find, book, rent a car—quick and super easy!</h1>
-        <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
-        <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}/>
+        <h1 className="hero__title">
+          Discover your next ride with ease and confidence.
+        </h1>
+
+        <p className="hero__subtitle">
+          DriveSphere simplifies your journey—explore, rent, or purchase
+          vehicles with unmatched ease and reliability.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
